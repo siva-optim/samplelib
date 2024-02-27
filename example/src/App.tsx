bb4ae82a8f04b7d60b2710aef676a6137f255cde
Sivasubramaniam <sivasubramaniam.v@theoptimum.tech>
2024-02-28 01:34:13 +0530

    feat: added pakcage build

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-samplelib';
+import { add } from 'react-native-samplelib';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    add().then(setResult);
   }, []);
 
   return (
