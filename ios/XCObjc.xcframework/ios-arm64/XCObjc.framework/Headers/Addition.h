84b2e16d127a1d88c7e4fec1752675842650b33c
Sivasubramaniam <sivasubramaniam.v@theoptimum.tech>
2024-02-28 01:03:22 +0530

    feat: init commit

---
+++ ios/XCObjc.xcframework/ios-arm64/XCObjc.framework/Headers/Addition.h
@@ -0,0 +1,17 @@
+//
+//  Addition.h
+//  XCObjc
+//
+//  Created by Designer326 on 27/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Addition : NSObject
+- (int)add;
+- (int)sub;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/XCObjc.xcframework/ios-arm64/XCObjc.framework/Headers/XCObjc.h
@@ -0,0 +1,18 @@
+//
+//  XCObjc.h
+//  XCObjc
+//
+//  Created by Designer326 on 27/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCObjc.
+FOUNDATION_EXPORT double XCObjcVersionNumber;
+
+//! Project version string for XCObjc.
+FOUNDATION_EXPORT const unsigned char XCObjcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCObjc/PublicHeader.h>
+
+#import <XCObjc/Addition.h>
